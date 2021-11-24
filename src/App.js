1472022c082176e4245ef46239527d340b6ae5e3
TMats <takomats@hotmail.com>
2021-11-24 17:22:00 -0500

    got my react environment working with starter files added.

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+    render() {
+        return (
+            <div className="App-title">Pokedex</div>
+        )
+    }
+}
+
+export default App;
