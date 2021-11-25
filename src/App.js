e95ba21a819bb53d673bfc0e9391e7eb4b38ed15
TMats <takomats@hotmail.com>
2021-11-24 19:20:08 -0500

    added defaultProp to Pokedex file.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Container, Row, Col } from 'react-bootstrap'
 import Pokecard from './Pokecard';
+import Pokedex from './Pokedex';
 import './App.css';
 
 class App extends Component {
@@ -10,10 +11,30 @@ class App extends Component {
                 <h1 className="App-title">Pokedex</h1>
                 <Container className="App-cards">
                     <Row className="App-first-row">
-                        <Col><Pokecard /></Col>
-                        <Col><Pokecard /></Col>
-                        <Col><Pokecard /></Col>
-                        <Col><Pokecard /></Col>
+                        <Col><Pokecard 
+                            id={Pokedex.id}
+                            name={Pokedex.name}
+                            type={Pokedex.type}
+                            base_experience={Pokedex.base_experience}
+                        /></Col>
+                        <Col><Pokecard 
+                            id={Pokedex.id}
+                            name={Pokedex.name}
+                            type={Pokedex.type}
+                            base_experience={Pokedex.base_experience}
+                        /></Col>
+                        <Col><Pokecard 
+                            id={Pokedex.id}
+                            name={Pokedex.name}
+                            type={Pokedex.type}
+                            base_experience={Pokedex.base_experience}
+                        /></Col>
+                        <Col><Pokecard 
+                            id={Pokedex.id}
+                            name={Pokedex.name}
+                            type={Pokedex.type}
+                            base_experience={Pokedex.base_experience}
+                        /></Col>
                     </Row>
                     <Row className="App-second-row">
                         <Col><Pokecard /></Col>
