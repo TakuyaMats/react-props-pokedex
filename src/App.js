0b52f99133d56180c1079689dcccea4bb9ebe23a
TMats <takomats@hotmail.com>
2021-11-27 21:23:12 -0500

    added the Pokegame logic to the application.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Container, Row, Col } from 'react-bootstrap'
 import Pokedex from './Pokedex';
+import Pokegame from './Pokegame';
 import './App.css';
 
 class App extends Component {
@@ -10,7 +11,7 @@ class App extends Component {
                 <h1 className="App-title">Pokedex</h1>
                 <Container className="App-cards">
                     <Row>
-                        <Col><Pokedex /></Col>
+                        <Col><Pokegame /></Col>
                     </Row>
                 </Container>
             </div>
